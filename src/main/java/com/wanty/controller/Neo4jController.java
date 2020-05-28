e28fbf02c92658ce74c1fe4f9ac0b2c91afe55f0
ArberChang <904105072@qq.com>
2020-05-28 09:12:25 +0800

    修改成最简单的实现方式

--- src/main/java/com/wanty/controller/Neo4jController.java
+++ src/main/java/com/wanty/controller/Neo4jController.java
@@ -1,51 +1,23 @@
 package com.wanty.controller;
 
-import com.wanty.entity.UserNode;
 import com.wanty.service.SupplyService;
-import org.apache.commons.lang3.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.UUID;
-
 @RestController
 public class Neo4jController {
 
-    @Autowired
-    private com.wanty.service.Neo4jService neo4jService;
 
     @Autowired
     private SupplyService supplyService;
 
-    //创建400个node
-    @RequestMapping(path = "/addUserNode", method = RequestMethod.GET)
-    public String addUserNode() {
-        int i = 0;
-        do {
-            final UserNode build = UserNode.builder().age(RandomUtils.nextInt(15, 40))
-                    .name("Fredia" + RandomUtils.nextInt(1, 1000))
-                    .userId(UUID.randomUUID().toString())
-                    .nodeId(RandomUtils.nextLong(1L, 999L)).build();
 
-            neo4jService.addUser(build);
-            i += 1;
-        } while (i < 100);
-
-        return "ok";
-    }
-
-    @RequestMapping(path = "/getUserNodeList", method = RequestMethod.GET)
-    public List<UserNode> getUserNodeList() {
-
-        return neo4jService.getUserNodeList();
-    }
 
     @RequestMapping(path = "/supply", method = RequestMethod.GET)
     public Boolean supply() {
-        supplyService.testService();
+        this.supplyService.testService();
         return true;
     }
 
--- src/main/java/com/wanty/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wanty.repository;
-
-import com.wanty.entity.UserNode;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface UserRepository extends Neo4jRepository<UserNode, Long> {
-}
